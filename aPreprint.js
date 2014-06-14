0157ff5bd0a2f16756bfc97637587deed5b473b6
Zach <taylorzr@gmail.com>
2014-06-14 09:46:25 -0500

    Cleaned up structure

--- aPreprint.js
+++
@@ -1,8 +0,0 @@
-// Acrobat all javascript files alphabetically. This needs to load first!
-
-//< Menu
-    submenu_name = "Preprint"
-    app.addSubMenu({
-		cName: submenu_name,
-		cParent: "Window"
-	});        	
